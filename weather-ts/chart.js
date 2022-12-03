3fe5ff5620af524e28c8ca62f8a90af0260642bb
vulpes37 <rjlevitt37@gmail.com>
2022-12-03 14:14:46 -0700

    Acessor functions

--- weather-ts/chart.js
+++ weather-ts/chart.js
@@ -4,6 +4,12 @@ async function drawLineChart() {
     // load data 
     const dataset = await d3.json("weather_data.json");
     console.table(dataset[0])
+
+    // use accessor functions to obtain relevant points
+    const yAccessor = d => d.temperatureMax
+    // use d3 to parse date
+    const dateParser = d3.timeParse("%Y-%m-%d") 
+    const xAccessor = d => dateParser(d.date)
 }  
 
 drawLineChart()
